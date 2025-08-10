874a017a1dd59d410eae20aab2a99c2a46161745
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2025-08-10 11:53:40 +0200

    Delete Tool directory

--- Tool/script_with_other_model/launch.sh
+++
@@ -1,3 +0,0 @@
-python main.py  test.txt --model distilroberta --output distilroberta_out.csv --cwe CWE-200
-python main.py  test.txt --model minilm --output minilm_out.csv --cwe CWE-200
-python main.py  test.txt --model mpnet --output mpnet_out.csv --cwe CWE-200
